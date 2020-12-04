12474d7ed1bc024dba74532c0821e6e51755caaf
Michael Shields <michael.shields@iwellsite.com>
2020-12-04 06:52:19 -0700

    Add stateless functional components, exact path matching for '/' and API fallback for dev-server config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 
-ReactDOM.render(<p>This is my React boilerplate project</p>, document.getElementById('app'));
+const DashboardPage = () => (
+    <div>
+        This is my Dashboard component
+    </div>
+);
+const AboutPage = () => (
+    <div>
+        This is my About component
+    </div>
+);
+const AddExpensePage = () => (
+    <div>
+        This is my Add Expense component
+    </div>
+);
+const EditExpensePage = () => (
+    <div>
+        This is my Edit Expense component
+    </div>
+);
+const HelpPage = () => (
+    <div>
+        This is my Help component
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path='/' exact={true} component={DashboardPage}/>
+            <Route path='/about' component={AboutPage}/>
+            <Route path='/create' component={AddExpensePage}/>
+            <Route path='/edit' component={EditExpensePage}/>
+            <Route path='/help' component={HelpPage}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
