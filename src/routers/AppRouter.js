f73ecd2b7f43b529f425ebe3e804214020c74980
Michael Shields <michael.shields@iwellsite.com>
2020-12-04 07:50:43 -0700

    Refactor existing routing to an AppRouter component

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
+// Try to workaround warning in browser console i.e. 
+// Warning: Please use `require("history").createBrowserHistory` instead of `require("history/createBrowserHistory")`. 
+// Support for the latter will be removed in the next major release.
+// From: webpack:///./node_modules/history/warnAboutDeprecatedCJSRequire.js?fee0
+// Google-suggested workaround: import { createBrowserHistory } from 'history'
+
+const DashboardPage = () => (
+    <div>
+        This is my Dashboard component
+    </div>
+);
+const AboutPage = () => (
+    <div>
+        This is my About component
+    </div>
+);
+const AddExpensePage = () => (
+    <div>
+        This is my Add Expense component
+    </div>
+);
+const EditExpensePage = () => (
+    <div>
+        This is my Edit Expense component
+    </div>
+);
+const HelpPage = () => (
+    <div>
+        This is my Help component
+    </div>
+);
+const NotFoundPage = () => (
+    <div>
+        404 - <Link to='/'>Go Home</Link>
+    </div>
+);
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to='/' activeClassName='is-active' exact={true}>Dashboard</NavLink>
+        <NavLink to='/about' activeClassName='is-active'>About</NavLink>
+        <NavLink to='/create' activeClassName='is-active'>Add Expense</NavLink>
+        <NavLink to='/edit' activeClassName='is-active'>Edit Expense</NavLink>
+        <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
+    </header>
+);
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header/>
+            <Switch>
+                <Route path='/' exact={true} component={DashboardPage}/>
+                <Route path='/about' component={AboutPage}/>
+                <Route path='/create' component={AddExpensePage}/>
+                <Route path='/edit' component={EditExpensePage}/>
+                <Route path='/help' component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
+        </div>
+    </BrowserRouter>
+);
+
+export default AppRouter;
